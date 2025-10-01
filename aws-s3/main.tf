c406a09008a16acfb179bbcc3d00653da6067553
trailblazer04 <leofrancis6988@gmail.com>
2025-10-01 20:54:44 +0530

    Created a file and uploaded it on s3 bucket and able to view and download

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -15,3 +15,10 @@ provider "aws" {
 resource "aws_s3_bucket" "leo-bucket" {
   bucket = "leo-s3-bucket-1234"
 }
+
+resource "aws_s3_object" "bucket-data" {
+  bucket = aws_s3_bucket.leo-bucket.bucket
+  source = "./myfile.txt"
+  key    = "mydata.txt"
+
+}

a73aae93930a8d250cf38fa0653f2b325a2cadb8
trailblazer04 <leofrancis6988@gmail.com>
2025-10-03 17:01:00 +0530

    Used the random provider to creat s3 buckets

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.14.1"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.2"
+    }
   }
 }
 
@@ -12,8 +16,12 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+resource "random_id" "rand_id" {
+  byte_length = 8
+}
+
 resource "aws_s3_bucket" "leo-bucket" {
-  bucket = "leo-s3-bucket-1234"
+  bucket = "leo-s3-bucket-${random_id.rand_id.hex}"
 }
 
 resource "aws_s3_object" "bucket-data" {
@@ -21,4 +29,8 @@ resource "aws_s3_object" "bucket-data" {
   source = "./myfile.txt"
   key    = "mydata.txt"
 
+}
+
+output "rand_id" {
+  value = random_id.rand_id.hex
 }
